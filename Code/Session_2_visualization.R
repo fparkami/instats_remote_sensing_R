9cb71ed2c849f6e444953f6767889f2feccb009a
Faranak Parkami <fparkami@asu.edu>
2025-09-29 00:12:11 -0700

    Update Session_2_visualization.R

--- Code/Session_2_visualization.R
+++ Code/Session_2_visualization.R
@@ -3,3 +3,36 @@
 # Packages needed
 library(terra)
 library(imageRy)
+
+# Packages needed
+install.packages("terra")
+#to install imageRy, you need to get it from github, therefore, you need to install devtools first
+install.packages("devtools")
+
+
+
+library(devtools)
+#devtools related functions to install packages from Github
+install_github("ducciorocchini/imageRy")
+
+#once imageRy has been installed by the devtools package,, it can be recalled by the library
+
+library(terra)
+
+
+library(imageRy)
+
+# Listing data
+im.list()  #lists all of the data
+
+#Importing Band 2
+b2 <- im.import("sentinel.dolomites.b2.tif")
+
+#install viridis package
+install.packages("viridis")
+library(viridis)
+plot(b2, col = magma(100))
+plot(b2, col = magma(150))     
+
+#Info about the band
+b2
