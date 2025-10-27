2104f40e11dc9dbea4f9b3e57c481a9c0e69305a
Faranak Parkami <fparkami@asu.edu>
2025-10-27 00:14:59 -0700

    Update and rename Session_4_Vegetation_Mapping.R to Session_4_Spectral_indices.R

---
+++ Code/Session_4_Spectral_indices.R
@@ -0,0 +1,41 @@
+# This code will be used to calculate the amount of biomass of plants
+
+library(terra)
+library(imageRy)
+
+# Listing images 
+im.list()
+
+# Importing the image -- this is from inside the package, from outside we should use rast
+m1992 <- im.import("matogrosso_l5_1992219_lrg.jpg")
+
+# the image of vegetation is red, so the NIR inferared is on top of the red:
+# NIR = band 1 (first component of the RGB) -- this is the most important one
+# red = band 2
+# green = band 3
+
+# to flip the image
+m1992f <- flip(m1992)
+plot(m1992f)
+
+# Another image
+m2006 <- im.import("matogrosso_ast_2006209_lrg.jpg")
+
+# to see the details of the images we just use their names
+m1992
+m2006
+
+# building the multiframe - put 1 image beside another
+par(mfrow = c(1 , 2))
+plot(m1992)
+plot(m2006)
+
+# making rgb images
+im.plotRGB (m1992, r = 1, g = 2, b =3)  #creates the exact same red image
+im.plotRGB (m2006, r = 1, g = 2, b =3)  #creates the exact same red image
+
+# Healthy vegetation appears bright red (since NIR is displayed in the red channel).
+# Urban areas and soil appear gray or brown.
+# Water appears dark or black (it absorbs NIR).
+
+
