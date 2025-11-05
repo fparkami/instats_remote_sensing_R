d1de07a80029b1077141cf7b240990e8b79a32ff
Faranak Parkami <fparkami@asu.edu>
2025-11-04 17:00:33 -0700

    Update Session_4_02_variability.R

--- Code/Session_4_02_variability.R
+++ Code/Session_4_02_variability.R
@@ -1,3 +1,9 @@
 # How to measure spatial variability (diversity or heterogeneity) 
 # if there is variability, it means there are several niches for wildlife to live in
 # We expect the higher variability, the higher the biodiversity (plants?) -- this is spectral variation hypothesis
+
+library(imageRy)
+library(terra)
+
+im.list()
+sent <- im.import("sentinel.png") 
