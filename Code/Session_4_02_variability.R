3117e71f91227edff5e3f5d8bad64a69161c00dc
Faranak Parkami <fparkami@asu.edu>
2025-11-09 20:29:09 -0700

    Update Session_4_02_variability.R

--- Code/Session_4_02_variability.R
+++ Code/Session_4_02_variability.R
@@ -6,4 +6,10 @@ library(imageRy)
 library(terra)
 
 im.list()
-sent <- im.import("sentinel.png") 
+sent <- im.import("sentinel.png")   #not sure why, but the instructor's code shows three bands, but mine is only one picture
+
+# NIR = band 1
+# red = band 2
+# green = band 3
+
+im.plotRGB(sent, r = 1, g = 2, b = 3)
